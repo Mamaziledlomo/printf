4b2d16fec424b9766d59ed95d34077aa2bb2d198
Mamaziledlomo <mamazilepdlomo@gmail.com>
2023-08-25 20:32:35 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -9,8 +9,8 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-	int i, printed - 0, printed_char - 0;
-	int flags, width, precision, size, buff_ind - 0;
+	int i, printed = 0, printed_char = 0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
@@ -19,13 +19,13 @@ int _printf(const char *format, ...)
 
 	va_start(list, format);
 
-	for (i = 0; format &&[i] != '\0'; i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
-			buffer[buff_ind++] - format[i];
+			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
-				printf_buffer(buffer, &buff_ind);
+				print_buffer(buffer, &buff_ind);
 
 			/* write(1, &format[i], 1);*/
 			printed_chars++;
@@ -40,7 +40,6 @@ int _printf(const char *format, ...)
 			++i;
 			printed = handle_print(format, &i, list, buffer,
 					flags, width, precision, size);
-
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
@@ -55,14 +54,14 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it ex list
+ * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
-		write(i, &buffer[0], *buff_ind);
+		write(1, &buffer[0], *buff_ind);
 
 	*buff_ind = 0;
 }
